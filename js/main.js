2aa2dd54630fa4955a7ae40e8b4ece13d6dcbd91
Dmitriy <osendiesesein@gmail.com>
2022-01-25 13:29:11 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,107 @@
+let ind = 0;
+let sumBetweenZeros = 0;
+let negativeNumbers = 0;
+let arr2 = [-22.38,41.16,0,37.12,432,27.92,0,-2.97];
+let roundedArray = [];
+let roundedUpArray = [];
+let lowest = arr2[0];
+let biggest = arr2.pop();
+let rounded = 0;
+let roundedUp = 0;
+
+arr2.forEach( (e) => {
+    if(e < 0){
+        negativeNumbers += e;
+    }
+    if ( e % 2 == 0){
+        if ( lowest > e){
+            lowest = e
+        }
+    }
+    if(biggest < e){
+        biggest = e;
+    }
+    if(arr2.length){
+        rounded = Math.round(e)
+        roundedArray.push(rounded);
+    }
+    if(arr2.length){
+        roundedUp = Math.ceil(e)
+        roundedUpArray.push(roundedUp);
+    }
+})
+
+
+// ДВА НУЛЯ
+// let arr3 = [];
+// let secondZeroInd = 0;
+// arr2.forEach( (element, index) => {
+//     if (element == '0'){
+//         secondZeroInd = index;
+//     }
+// });
+// arr3 = arr2.slice(secondZeroInd, secondZeroInd)
+// console.log(arr3)
+
+
+let positiveArray = [];
+arr2.map( (e) => {
+    if( e > 0){
+        positiveArray.push(e)
+    }
+})
+console.log('Сумма отрицательных чисел: ',negativeNumbers)
+console.log('Наименьшее четное число: ',lowest)
+indMax = arr2.indexOf(biggest)
+indLow = arr2.indexOf(lowest)
+indSum = indMax + indLow;
+console.log('Сумма номеров минимального и максимального элементов: ',indSum)
+console.log('Положительные числа из массива:',positiveArray)
+console.log('Округленные числа: ',roundedArray);
+console.log('Округленные числа вверх: ',roundedUpArray);
+
+
+
+
+
+let moreThan5 = 0;
+let digits = 0;
+
+let str = 'Lorem Ipsum is simply du5mmy text of the printing and typesetting industry. Lorem Ips1um has been the industrys standard du2mmy text ever since the 1s, when an unknown printer took a galley of type and scram4bled it to make a type spe6cimen book. It has survived not only five cen8turies, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 9s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum';
+let splitted = str.split(' ')
+console.log('Кол-во слов в строке:', splitted.length);
+
+let splittedE = [];
+splitted.forEach( (e) => {
+    if ( e.length > 4){
+        moreThan5 += 1;
+    }
+})
+
+splittedE = splitted.map( (e) => {
+    return e.split('')
+})
+console.log(splittedE)
+splittedE.forEach( (e) =>{
+    if ( Number.isInteger(e)){
+        digits += e
+    }
+});
+console.log('Количество слов , содержащие больше 5 букв : ', moreThan5)
+console.log(digits)
+
+
+smallLetter = splitted.map( (e) => {
+    return e.toLowerCase()
+});
+
+console.log('Все буквы строчные: ',smallLetter)
+let middleLetter;
+withoutMiddleLetter = splitted.forEach( (e) => {
+    if( e.length % 2 !== 0){
+        middleLetter = (e.length / 2) + 1
+        withoutMiddleLetter = e.slice(middleLetter,middleLetter)
+    }
+});
+console.log(withoutMiddleLetter)
+
